8a1356daf9d3444a9f2aa8ac6c61e7e7d0400bea
Cathy Liu <36806135+catliugit@users.noreply.github.com>
2024-07-27 16:12:39 -1000

    Commit File Path Definitions and Relevant Columns

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,1 +1,22 @@
 import csv
+
+# Define the CSV file paths
+file_path = r"C:\Users\025445\Desktop\Tableau Projects\Side Projects\Disability\DHDS_-_Prevalence_of_Disability_Status_and_Types_20240721.csv"
+cleaned_file_path = r"C:\Users\025445\Desktop\Tableau Projects\Side Projects\Disability\disability_data_clean.csv"
+
+relevant_columns = [
+    "Year",
+    "LocationAbbr",
+    "LocationDesc",
+    "Indicator",
+    "Response",
+    "Data_Value_Unit",
+    "Data_Value_Type",
+    "Data_Value",
+    "Number",
+    "WeightedNumber",
+    "Low_Confidence_Limit",
+    "High_Confidence_Limit",
+    "StratificationCategory1",
+    "Stratification1"
+]
